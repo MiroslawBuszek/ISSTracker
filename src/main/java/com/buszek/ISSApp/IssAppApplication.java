a6c27ef3dee9454af730db1e14d264103dbe0eae
Mirosław Buszek <zoowett@gmail.com>
2019-09-27 22:25:01 +0200

    Initial commit

---
+++ src/main/java/com/buszek/ISSApp/IssAppApplication.java
@@ -0,0 +1,13 @@
+package com.buszek.ISSApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssAppApplication.class, args);
+	}
+
+}
