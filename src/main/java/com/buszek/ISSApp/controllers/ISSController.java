5df4b10fca5b2a45a90149024ae88451d2106e9a
Mirosław Buszek <zoowett@gmail.com>
2019-09-27 23:57:33 +0200

    Change class name toisslocator

--- src/main/java/com/buszek/ISSApp/controllers/ISSController.java
+++ src/main/java/com/buszek/ISSApp/controllers/ISSController.java
@@ -1,5 +1,6 @@
 package com.buszek.ISSApp.controllers;
 
+import com.buszek.ISSApp.models.IssLocator;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +10,8 @@ import org.springframework.web.client.RestTemplate;
 @RequestMapping("/api")
 public class ISSController {
     @GetMapping("/issLocation")
-    public String getISSLocation(){
+    public IssLocator getISSLocation() {
         RestTemplate restTemplate = new RestTemplate();
-        return restTemplate.getForObject("http://api.open-notify.org/iss-now.json", String.class);
+        return restTemplate.getForObject("http://api.open-notify.org/iss-now.json", IssLocator.class);
     }
 }
