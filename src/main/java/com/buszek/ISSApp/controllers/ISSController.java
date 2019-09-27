118c12f53b3526ddad706d536e219b564a80d768
Mirosław Buszek <zoowett@gmail.com>
2019-09-27 23:24:57 +0200

    Add iss-api url

--- src/main/java/com/buszek/ISSApp/controllers/ISSController.java
+++ src/main/java/com/buszek/ISSApp/controllers/ISSController.java
@@ -3,12 +3,14 @@ package com.buszek.ISSApp.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/api")
 public class ISSController {
     @GetMapping("/issLocation")
     public String getISSLocation(){
-        return "Hello ISS";
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate.getForObject("http://api.open-notify.org/iss-now.json", String.class);
     }
 }
