755693c34734b7ca5c25e0885dd57c3f4d30bada
Mirosław Buszek <zoowett@gmail.com>
2019-09-28 19:11:59 +0200

    Add error handler based on HystrixCommand

---
+++ src/main/java/com/buszek/ISSApp/service/LocationService.java
@@ -0,0 +1,24 @@
+package com.buszek.ISSApp.service;
+
+import com.buszek.ISSApp.models.IssLocator;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class LocationService {
+    private static final String FAILED = "failed";
+
+    @HystrixCommand(fallbackMethod = "fallback")
+    public IssLocator getIssLocation() {
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate.getForObject("http://api.open-notify.org/iss-now.json", IssLocator.class);
+    }
+
+    public IssLocator fallback() {
+        IssLocator issLocator = new IssLocator();
+        issLocator.setMessage(FAILED);
+        return issLocator;
+    }
+}
+
