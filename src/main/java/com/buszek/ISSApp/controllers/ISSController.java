99f3623d36e293ae354beba0ac56d7833b725041
Mirosław Buszek <zoowett@gmail.com>
2019-09-28 23:58:59 +0200

    Add @CrossOrigin annotation

--- src/main/java/com/buszek/ISSApp/controllers/ISSController.java
+++ src/main/java/com/buszek/ISSApp/controllers/ISSController.java
@@ -3,6 +3,7 @@ package com.buszek.ISSApp.controllers;
 import com.buszek.ISSApp.models.IssLocator;
 import com.buszek.ISSApp.service.LocationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,10 +13,11 @@ import org.springframework.web.client.RestTemplate;
 @RequestMapping("/api")
 public class ISSController {
 
-    @Autowired
+     @Autowired
     private LocationService locationService;
 
     @GetMapping("/issLocation")
+    @CrossOrigin
     public IssLocator getISSLocation() {
        return this.locationService.getIssLocation();
 
